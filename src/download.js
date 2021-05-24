f82ac1d1025aafd06ee3fe6422ef8636ef480496
Tanay Maheshwari <tanaymodani18@gmail.com>
2021-05-24 14:56:02 +0530

    added message.js

---
+++ src/download.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const Buffer = require("buffer").Buffer;
+const tracker = require("./tracker");
+
+module.exports = (torrent) => {
+  tracker.getPeers(torrent, (peers) => {
+    peers.forEach(download);
+  });
+};
+
+function download(peer) {
+  const socket = net.Socket();
+  socket.on("error", console.log);
+  socket.connect(peer.port, peer.ip, () => {
+    //socket.write.. write message here
+  });
+  socket.on("data", (data) => {
+    //handle reponse here
+  });
+}
